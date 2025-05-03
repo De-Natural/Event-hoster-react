536d8c87e5eb6602411876985c33758ec8f75f95
festus <festuschidubem376@gmail.com>
2025-05-03 08:51:28 -0700

    optimizing my page

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Faq from '../components/faq/Faq'
+import SignUp from '../components/auth-pages/SignUp'
+import Login from '../components/auth-pages/login/Login'
 
 export const IndexRoutes = () => {
   return (
     <Routes>
       <Route path="/faq" element={<Faq />} />
+      <Route path="/auth-pages" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
